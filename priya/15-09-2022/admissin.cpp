bad4ff340157237afe94ad18b8d32e99108a5fe7
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-16 21:59:39 +0530

    Create admissin.cpp

---
+++ priya/15-09-2022/admissin.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string name,city , branch;
+  
+  cin>>name>>city>>branch;
+  cout<<"my name is "<<name<<"i am from" <<city<<"my branch is"<<branch;
+}
+  
