ced42b69b620155ae86c01fa357d19244f5e4f14
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-17 18:17:00 +0530

    Update tableofanynumber.cpp

--- priya/16-09-2022/tableofanynumber.cpp
+++ priya/16-09-2022/tableofanynumber.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
+    cout<<"write any number whose table is to be printed : ";
     int n;
     cin>>n;
-    cout<<"Table of";
+    cout<<"Table of "<<n<<"is" ;
     for(int i=1;i<11;i++)
     {
-    cout<<n<<"*"<<i<<"="<<n*i<<endl;
+    cout<<"\n"<<n<<"*"<<i<<"="<<n*i<<endl;
     }
 
     return 0;
 }
+
+    
+
