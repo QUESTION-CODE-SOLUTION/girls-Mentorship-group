fe9212453650a93a7aab736f0f49096b3b9ce4dc
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-17 18:36:47 +0530

    Create tableofanynumberusingwhileloop.cpp

---
+++ priya/16-09-2022/tableofanynumberusingwhileloop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"write any number whose table is to be printed : ";
+    int n;
+    cin>>n;
+    cout<<"Table of"<<n<<"is" 
+    int i=1;
+    while(i<11)
+    {
+    cout<<"\n"<<n<<"*"<<i<<"="<<n*i<<endl;
+          i++;
+    }
+
+    return 0;
+}
