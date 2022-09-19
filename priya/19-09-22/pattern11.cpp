0f409be0d3ef2312f0955cfa1e45f38aaa5944fa
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-20 00:11:51 +0530

    Update and rename .cpp to pattern11.cpp

---
+++ priya/19-09-22/pattern11.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   for(int i=5;i>=1;i--)
+     { 
+          for(int j=1;j<=i;j++)
+      {
+            cout<<j;
+      }
+      cout<<endl;
+     }
+    return 0;
+}
+
+
+output:-
+  
+12345
+1234
+123
+12
+1  
