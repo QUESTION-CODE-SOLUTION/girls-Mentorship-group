d5835f5d90acf0fa8a4f1bd556a3afc396aeb85e
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-19 23:50:23 +0530

    Create pattern5.cpp

---
+++ priya/19-09-22/pattern5.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   for(int i=1;i<=8;i++)
+     { {
+          for(int j=1;j<=8;j++)
+      
+      {
+       if(i%2==0||j%2==0)
+          cout<<"#";
+       
+       else
+          cout<<"*";
+      }
+      }
+      cout<<endl;
+     }
+    return 0;
+}
+
+
+output:-
+  
+*#*#*#*#
+########
+*#*#*#*#
+########
+*#*#*#*#
+########
+*#*#*#*#
+########  
