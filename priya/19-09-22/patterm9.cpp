23afda2ade8bf7b195fc3484c92cf0a7358010a8
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-20 00:01:22 +0530

    Create patterm9.cpp

---
+++ priya/19-09-22/patterm9.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   for(int i=1;i<=8;i++)
+     { 
+          for(int j=1;j<=i;j++)
+      {
+            cout<<i;
+      }
+      cout<<endl;
+     }
+    return 0;
+}
+
+
+output:-
+  
+1
+22
+333
+4444
+55555
+666666
+7777777
+88888888
+  
