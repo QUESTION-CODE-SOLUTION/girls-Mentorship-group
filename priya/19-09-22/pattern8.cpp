55f133bc0eb114294825e1aafc74fd90876c1b5b
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-19 23:56:04 +0530

    Create pattern8.cpp

---
+++ priya/19-09-22/pattern8.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   for(int i=1;i<=8;i++)
+     { 
+          for(int j=1;j<=i;j++)
+      {
+            cout<<j;
+      }
+      cout<<endl;
+     }
+    return 0;
+}
+
+output:-
+1
+12
+123
+1234
+12345
+123456
+1234567
+12345678
+
+  
