f9eeb0be4f4d39183e4996a4a7be19cff7f9726b
PrernaRathore10 <113295862+PrernaRathore10@users.noreply.github.com>
2022-09-19 23:59:06 +0530

    Create 19-09-2022.cpp

---
+++ prena/19-09-2022.cpp
@@ -0,0 +1,23 @@
+/* object- to print the pattern
+* * * * * 
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+*/
+
+
+#include <iostream>
+using namespace std;
+int main(){
+  int i,n,j;
+  cout<<"Number :";
+  cin>>n;
+  for (int i=1;i<=n;i++){
+    for (int j=1;j<=n;j++){
+      cout<<"* ";
+    }
+    cout<<endl;
+  }
+  return 0;
+}
