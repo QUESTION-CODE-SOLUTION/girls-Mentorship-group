522d0b2ff1c6087328a9870633a1391da78489c5
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-19 23:43:30 +0530

    Create pattern2.cpp

---
+++ priya/19-09-22/pattern2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   for(int i=1;i<=8;i++)
+     { {
+          for(int j=1;j<=8;j++)
+      
+      {
+       cout<<j;
+      }
+      }
+      cout<<endl;
+     }
+    return 0;
+}
+
+output:-
+  
+12345678
+12345678
+12345678
+12345678
+12345678
+12345678
+12345678
+12345678
