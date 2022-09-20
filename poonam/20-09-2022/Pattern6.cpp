6f2c82882156418c877c06749f58bc56116aa86f
Poonamraut040 <113535198+Poonamraut040@users.noreply.github.com>
2022-09-21 00:00:15 +0530

    Create Pattern6.cpp

---
+++ poonam/20-09-2022/Pattern6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    for(int i=1;i<=5;i++)
+    {
+        for ( int j=5; j>=i;j--)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}        
+
+
+
+
+Comment: 54321
+         5432
+         543
+         54
+         5
