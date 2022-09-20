29bebbfcc2c627f171dae7efa223f33c14ddf54b
shraddha tiwari <113535239+shraddhatiwarii@users.noreply.github.com>
2022-09-21 00:07:54 +0530

    Create shraddhapattern2.cpp

---
+++ shraddha/20-09-2022/shraddhapattern2.cpp
@@ -0,0 +1,27 @@
+Display"*****
+*   *
+*   *
+*   *
+*****
+Ans. #include <iostream>
+using namespace std;
+int main() {
+    for(int i=1; i<=5;i++)
+    {
+        for( int j=1; j<=5 ;j++)
+        {
+            if( i==1 || j==1 ||i==5 || j==5)
+            {
+                cout<<"*";
+            }
+            else
+            {
+            cout<<" ";
+            }
+        
+        }
+        
+        cout<<endl;
+     }
+     
+    }
