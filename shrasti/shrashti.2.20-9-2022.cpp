367344f198d338cab2c879b2e7f64886f3496052
shrashti2004 <113536361+shrashti2004@users.noreply.github.com>
2022-09-20 14:10:38 +0530

    Create shrashti.2.20-9-2022.cpp

---
+++ shrasti/shrashti.2.20-9-2022.cpp
@@ -0,0 +1,21 @@
+//Print pattern 
+//1234
+//1234
+//1234
+//1234
+
+#include<iostream>
+using namespace std;
+ int main()
+{
+for(int i = 1;i<=4;i++)
+{
+for (int j=1 ; j<=4;j++)
+{
+cout<<j;
+
+}
+cout<<"\n";
+}
+return 0;
+}
