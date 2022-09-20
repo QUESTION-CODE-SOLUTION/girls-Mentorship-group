605aedf02a32b18bcc58eefbc93d7f36df84b690
Vaishnavi2445 <113535692+Vaishnavi2445@users.noreply.github.com>
2022-09-20 12:23:17 +0530

    Create Vaishnavi20-9-22.cpp

---
+++ vaishnavi/Vaishnavi20-9-22.cpp
@@ -0,0 +1,21 @@
+// print pattern
+  //  ****
+  //  ****
+  //  ****
+  //  ****
+  
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i , j ;
+    for(i=1;i<=4;i++)
+    {
+     for(j=1;j<=4;j++)
+     {
+       cout<<"*";
+     }
+       cout<<endl;
+    }
+}
