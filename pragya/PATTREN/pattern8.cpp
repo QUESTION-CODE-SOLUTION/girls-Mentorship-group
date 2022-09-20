f4c5a5dc9c4b98394b01d43bcc593f01b27d80f0
Pragya0115 <109800495+Pragya0115@users.noreply.github.com>
2022-09-20 18:55:51 +0530

    Create pattern8.cpp

---
+++ pragya/PATTREN/pattern8.cpp
@@ -0,0 +1,19 @@
+/*  * * * * *
+    * * * *
+    * * *
+    * *
+    *    */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i =5; i>=1;i--)
+    {
+        cout<<endl;
+        for(int j=i;j>=1;j--)
+        {
+            cout<<"*";
+        }
+    }
+}
