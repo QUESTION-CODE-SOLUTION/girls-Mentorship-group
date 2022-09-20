4db5ba317461716b9f0b4fa5e36eaff9f519a023
shrashti2004 <113536361+shrashti2004@users.noreply.github.com>
2022-09-20 12:26:01 +0530

    Create shrashti20-9-2022.cpp

---
+++ shrasti/shrashti20-9-2022.cpp
@@ -0,0 +1,26 @@
+//Print patter 
+//   ****
+ //  ****
+//   ****
+//   ****    
+     
+
+
+#include<iostream>
+using namespace std;
+ int main()
+{
+for(int i = 1;i<=4;i++)
+{
+for (int j=1 ; j<=4;j++)
+{
+cout<<"*";
+
+}
+cout<<"\n";
+}
+return 0;
+}
+
+
+    
