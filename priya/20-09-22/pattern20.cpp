20764efd86340172afc871743c25367ae2c1411b
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-20 23:49:45 +0530

    Create pattern20.cpp

---
+++ priya/20-09-22/pattern20.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=1;i<=10;i++)
+    {
+        for(int j=1;j<=10-i;j++)
+        {
+           cout<<" ";
+        }
+        for(int k=1;k<=i;k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    {
+        for(int i=9;i>=1;i--)
+        {
+            for(int j=1;j<=10-i;j++)
+           {
+            cout<<" ";
+           }
+           for(int k=1;k<=i;k++)
+           {
+               cout<<"*";
+           }
+        cout<<endl;
+        }
+    }
+    return 0;
+}
+
+
+output:-
+  
+  
+         *
+        **
+       ***
+      ****
+     *****
+    ******
+   *******
+  ********
+ *********
+**********
+ *********
+  ********
+   *******
+    ******
+     *****
+      ****
+       ***
+        **
+         *
+
+  
