b4ea2d4d98cac180e3d851655ccd66e577c09ab3
Pragya0115 <109800495+Pragya0115@users.noreply.github.com>
2022-09-20 18:45:03 +0530

    Create pattern4.cpp

---
+++ pragya/PATTREN/pattern4.cpp
@@ -0,0 +1,21 @@
+/*  * # * #
+    * # * #
+    * # * # 
+    * # * # */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i =1; i<5;i++)
+    {
+        cout<<endl;
+        for(int j=1;j<5;j++)
+        {
+            if(j%2==0)
+                cout<<"#";
+            else
+                cout<<"*";
+        }
+    }
+}
