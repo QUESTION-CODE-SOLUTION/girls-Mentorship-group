cfadb87ac0992c0f31118cf21dc90ef7eecb76ac
shraddha tiwari <113535239+shraddhatiwarii@users.noreply.github.com>
2022-09-20 23:36:43 +0530

    Update shraddhapattern.cpp

--- shraddha/20-09-2022/shraddhapattern.cpp
+++ shraddha/20-09-2022/shraddhapattern.cpp
@@ -1,1 +1,28 @@
+1. Display 1
+01
+010
+1010
+10101
 
+Ans. #include <iostream>
+using namespace std;
+int main() {
+    int c=1;
+    for(int i=1; i<=5;i++)
+    {
+        for( int j=1; j<=i ;j++)
+        {
+            if(c%2==0)
+            {
+                cout<<"0";
+            }
+            else{
+            cout<<"1";
+            }
+            c++;
+        }
+        
+        cout<<endl;
+     }
+     
+    }
