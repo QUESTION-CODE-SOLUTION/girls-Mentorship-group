e3a953907a20fdf36b81426957e58d9aa8b4e42e
Pragya0115 <109800495+Pragya0115@users.noreply.github.com>
2022-09-20 19:12:22 +0530

    Create pattern10.cpp

---
+++ pragya/PATTREN/pattern10.cpp
@@ -0,0 +1,20 @@
+/* 1 
+   2 2 
+   3 3 3 
+   4 4 4 4
+   5 5 5 5 5
+    */
+
+#include<iostream>
+using namespace std; 
+int main()
+{
+    for(int i =1; i<= 5;i++)
+    {
+        cout<<endl;
+        for(int j=1;j<=i;j++)
+        {
+            cout<<i;
+        }
+    }
+}
