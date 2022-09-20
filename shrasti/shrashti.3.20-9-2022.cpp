ece5916327bd81f0702d6addca13ab1e7c3bf5fe
shrashti2004 <113536361+shrashti2004@users.noreply.github.com>
2022-09-20 14:12:13 +0530

    Create shrashti.3.20-9-2022.cpp

---
+++ shrasti/shrashti.3.20-9-2022.cpp
@@ -0,0 +1,20 @@
+//1111
+//2222
+//3333
+//4444
+
+#include<iostream>
+using namespace std;
+ int main()
+{
+for(int i = 1;i<=4;i++)
+{
+for (int j=1 ; j<=4;j++)
+{
+cout<<i;
+
+}
+cout<<"\n";
+}
+return 0;
+}
