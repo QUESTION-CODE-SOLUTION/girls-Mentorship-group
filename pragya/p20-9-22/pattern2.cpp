8969f42712ab4d4cda5e0fbfbd04d04aaa49a63b
Pragya0115 <109800495+Pragya0115@users.noreply.github.com>
2022-09-21 18:20:49 +0530

    Create pattern2.cpp

---
+++ pragya/p20-9-22/pattern2.cpp
@@ -0,0 +1,25 @@
+/*
+    *
+   **
+  ***
+ ****
+*****
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+for(int i=1;i<=5;i++)
+{
+    for(int j=5-i;j>=1;j--)
+    {
+        cout<<" ";
+    }
+    for(int k=1;k<=i;k++)
+    {
+        cout<<"*";
+    }
+    cout<<endl;
+}
+}
