aac18ac0c761089423684ab717293f87797c9ad7
shrashti2004 <113536361+shrashti2004@users.noreply.github.com>
2022-09-23 01:34:29 +0530

    Create shrashti23-9-22.cpp

---
+++ shrasti/shrashti23-9-22.cpp
@@ -0,0 +1,25 @@
+/* for n number rows amd column
+****
+*#*#
+****
+*#*# */
+#include <iostream>
+using namespace std ;
+int main()
+{
+    int n;
+    cin>>n;
+ for (int i = 1;i<n;i++)
+ {
+for (int j=1;j<n;j++)
+{ if( i%2==0&&j%2==0)    
+
+ 
+    cout<<"#";
+
+
+else cout<<"*";}
+cout<<"\n";
+
+
+} }
