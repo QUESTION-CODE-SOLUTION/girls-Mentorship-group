c09a3b37982e1468fc70b9369c5a4e335128ce9d
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-23 00:43:40 +0530

    Create factorial.cpp

---
+++ priya/21-09-22/factorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int factorial (int n)
+{
+    
+    int a=1;
+    for(int i=1;i<=n;i++)
+    
+    {
+        a=a*i;
+        
+    }
+    cout<<a<<endl;
+
+    return a;
+}
+int main()
+{
+    factorial (4);
+    
+}
+
+
+output:-
+24  
