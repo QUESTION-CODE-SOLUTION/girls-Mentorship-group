6646a98ba6bf64fc3380b9ad2d20f5948a440921
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-23 00:11:20 +0530

    Create function2.cpp

---
+++ priya/21-09-22/function2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void introduction(string name ,string branch,string city,float CGPA)
+
+{
+    cout<<"My name is " <<name<<"."<<endl;
+    cout<<"Branch is "<<branch<<"."<<endl;
+    cout<<"I am from "<<city<<endl;
+    cout<<"My 1st year CGPA was "<<CGPA<<endl;
+}
+int main()
+{
+    introduction("Priya Jain","Computer science engineering","Damoh",8.2);
+}
+
+
+
+
+
+output:-
+  
+  
+My name is Priya Jain.
+Branch is Computer science engineering.
+I am from Damoh
+My 1st year CGPA was 8.2
+
