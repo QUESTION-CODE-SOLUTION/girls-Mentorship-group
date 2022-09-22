5655a61140c41c2d7e3019e75a9b8077d4600c7c
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-22 23:56:16 +0530

    Create functions.cpp

---
+++ priya/21-09-22/functions.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+void introduction()
+
+{
+    cout<<"My name is Priya Jain. "<<endl;
+    cout<<"Branch is Computer Science Engineering. "<<endl;
+    cout<<"I am from Damoh (M.P.). "<<endl;
+    cout<<"My CGPA was 8.2"<<endl;
+    cout<<"I had done my schooling from St. John's Hr. Sec. school ,damoh. "<<endl;
+    cout<<"My 12th percentage was 91.4%."<<endl;
+}
+int main()
+{
+    introduction();
+}
+
