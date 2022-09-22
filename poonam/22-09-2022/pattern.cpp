2384eb597bf140d280cb4ef8a7bd563c2b5b707b
Poonamraut040 <113535198+Poonamraut040@users.noreply.github.com>
2022-09-22 09:08:06 +0530

    Create pattern.cpp

---
+++ poonam/22-09-2022/pattern.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+     for (int i=0; i<=5; i++)
+    {
+        for (int j=0;j<=10;j++)
+        {
+          if( i+j<5)  
+          {
+              cout<<"*";
+          }
+          else
+          {
+              cout<<" ";
+          }
+          if(i+5<j)
+          {
+              cout<<"*";
+          }
+          else
+          {
+              cout<<" ";
+          }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+Comment: * * * * *    * * * * *
+         * * * *        * * * *
+         * * *            * * *
+         * *                * *
+         *                    *
