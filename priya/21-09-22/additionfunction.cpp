a42623e727a151b2811d46dc8887d703b8365375
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-23 00:31:33 +0530

    Create additionfunction.cpp

---
+++ priya/21-09-22/additionfunction.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void Sum(int a, int b)
+
+{
+    cout<<"a+b"<<endl;
+    return ;
+}
+int suum (int a, int b)
+{
+    return a+b;
+}
+int main()
+{
+    suum(8,9);
+    suum(45,56);
+    int a =suum(4,9);
+    int g=suum(356,44);
+    cout<<suum(8,9)<<endl;
+    cout<<suum(45,56)<<endl;
+    cout<<a<<endl;
+    cout<<g<<endl;
+}
+
+
+
+
+output:-
+  
+1
+101  
+13
+400
