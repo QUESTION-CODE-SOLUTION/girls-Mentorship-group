367f7fff41a0eb9cac290770eee2ad5621966f5b
shraddha tiwari <113535239+shraddhatiwarii@users.noreply.github.com>
2022-09-22 17:25:34 +0530

    Create shraddhastarPattern.cpp

---
+++ shraddha/shraddhastarPattern.cpp
@@ -0,0 +1,28 @@
+display;
+****
+***
+**
+*
+Ans. #include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i=1; i<=5; i++)
+    {
+        for (int j=5;j<=1;j++)
+        {
+          cout<<" ";
+          }
+          for (int k=5-i;k>=1;k--)
+          {
+              cout<<"*";
+          }
+          cout<<endl;
+           }
+        
+    }
+
+
+
+
