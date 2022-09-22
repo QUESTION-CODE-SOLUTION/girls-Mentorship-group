587c7e0736561a2b6485b063fe9b8738b8eb94ff
shrashti2004 <113536361+shrashti2004@users.noreply.github.com>
2022-09-23 02:37:17 +0530

    Create shrashti10_ -23-9-22.cpp

---
+++ shrasti/shrashti10_ -23-9-22.cpp
@@ -0,0 +1,21 @@
+/* 
+12345
+1234
+123
+12
+1
+*/
+#include <iostream>
+using namespace std;
+int main ()
+{
+    for(int i=5;i>=i;i--)
+ {
+   for(int j=1;j<=i;j++)
+   {
+    cout<<j;
+   }
+   cout<<"\n";
+
+    }
+}
