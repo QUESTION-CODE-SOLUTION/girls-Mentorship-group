20e03d00209ce62dd9f3c80d7f68598922c30e76
Poonamraut040 <113535198+Poonamraut040@users.noreply.github.com>
2022-09-23 19:01:56 +0530

    Create arrary1.cpp

---
+++ poonam/23-09-2022/arrary1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int arr[10];
+    for ( int i=0; i<10;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<arr[i]<<" "<<endl;
+    return 0;
+    
+}
+
+
+
+Comment: 10
+         20
+         30
+         40
+         50
+         60
+         70
+         80
+         90
+         100
