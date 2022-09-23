0ff19ff10b2cb62d9b2862e440a41d9de79c8308
shraddha tiwari <113535239+shraddhatiwarii@users.noreply.github.com>
2022-09-23 17:18:14 +0530

    Create function.cpp

---
+++ shraddha/function.cpp
@@ -0,0 +1,19 @@
+print sum of two numbers using functions.
+  Ans. #include <iostream>
+using namespace std;
+void summ (int a, int b)
+{
+    cout<< a+b;
+}
+int suum (int a, int b)
+{
+ return a+b;
+}
+int main()
+{
+    summ(5,2);
+    suum(4,8);
+    int a= suum(5,2);
+    cout<<endl;
+    cout<<suum(4,8);
+}
