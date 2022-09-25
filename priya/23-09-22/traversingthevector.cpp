21c80964cf695068785015b835bc3f267b03fae7
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-25 16:28:30 +0530

    Create traversingthevector.cpp

---
+++ priya/23-09-22/traversingthevector.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+   vector<int>vec={10,20,30,40,50};
+   
+    for(int i=0;i<vec.size();i++)
+    {
+         cout<<vec[i]<<" ";
+    }
+}
+
+
+output:-
+  
+10 20 30 40 50 
