8d84cdf4d5c7e7f09982617728518fde670a15b5
poonam <113535198+Poonamraut040@users.noreply.github.com>
2022-09-25 16:02:29 +0530

    Create function3.cpp

---
+++ poonam/25-09-2022/function3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int factorial( int n)
+{
+    int a=1;
+    for(int i=1;i<=n;i++)
+    {
+        a=a*i;
+    }
+    cout<<a<<endl;
+    return a;
+}
+int main()
+{
+    factorial(6);
+    
+}
+
+
+COMMENT: 720
+  
