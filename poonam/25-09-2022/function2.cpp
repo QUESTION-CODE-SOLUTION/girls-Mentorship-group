400144ee7e212870b71a8c4d175d48f0216792b1
poonam <113535198+Poonamraut040@users.noreply.github.com>
2022-09-25 15:25:19 +0530

    Create function2.cpp

---
+++ poonam/25-09-2022/function2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void suum(int a, int b)
+{
+ cout<<"a+b = "<<endl;
+ return ;
+}
+int summ(int c ,int d)
+{
+    cout<<"c+d=";
+    return c+d;
+}
+int main()
+{
+    suum(8,9);
+  
+    cout<<summ(5,5)<<endl;
+}
+
+
+COMMENT: a+b=
+         c+b=10
