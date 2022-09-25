2cd8391310f6fcd5b0acf8dfb15a4cdad221b42b
poonam <113535198+Poonamraut040@users.noreply.github.com>
2022-09-25 19:26:00 +0530

    Create primenumber.cpp

---
+++ poonam/25-09-2022/primenumber.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void primenumber( int n)
+{
+    for( int i=2; i<n; i++)
+    {
+        if (n%i==0)
+        {
+            cout<<n<<" is not prime number"<<endl;
+            break;
+        }
+        else
+        {
+            cout<<n<<" is prime number"<<endl;
+        }
+        return;
+    }
+}
+int main()
+{
+    primenumber(6);
+}
+
+
+
+COMMENT : 6 is not a prime number.
