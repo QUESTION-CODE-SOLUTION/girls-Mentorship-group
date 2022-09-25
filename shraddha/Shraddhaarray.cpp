3efbc2f9a5ba5da8966aa9fbe598551d7015218d
shraddha tiwari <113535239+shraddhatiwarii@users.noreply.github.com>
2022-09-25 14:41:57 +0530

    Create Shraddhaarray.cpp

---
+++ shraddha/Shraddhaarray.cpp
@@ -0,0 +1,25 @@
+Display:- 
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+Ans:-  #include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int arr[10];
+    for ( int i=0; i<10;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<arr[i]<<" "<<endl;
+    return 0;
+    
+}
