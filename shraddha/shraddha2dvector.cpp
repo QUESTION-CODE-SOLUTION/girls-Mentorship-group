b647b60a2702b854d1653bec2aa15fdd33157c32
shraddha tiwari <113535239+shraddhatiwarii@users.noreply.github.com>
2022-09-26 00:55:39 +0530

    Create shraddha2dvector.cpp

---
+++ shraddha/shraddha2dvector.cpp
@@ -0,0 +1,26 @@
+display
+1 2 3 
+4 5 6 
+7 8 9   using vectors
+Ans. #include <vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+void display(vector<vector<int>>vec)
+{
+    for(int i=0;i<vec.size();i++)
+    {
+        for(int j=0;j<vec[i].size();j++)
+        {
+            cout<<vec[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    vector<vector<int>>vec={{1,2,3},{4,5,6},{7,8,9}};
+    display (vec);
+}
