1dfde8a5dd45c58d7ae7a15876f649dd3e1259d2
SONIYAPATIDAR <110100877+SONIYAPATIDAR@users.noreply.github.com>
2022-09-25 13:59:50 +0530

    Add files via upload

---
+++ soniya/function.cpp
@@ -0,0 +1,75 @@
+/*#include <iostream>
+using namespace std;
+void introduction()
+{
+  cout<<"MY name is soniya"<<endl;
+  cout<<"branch is CSE"<<endl;
+  cout<<"CGPA is __"<<endl;
+
+}
+
+int main() {
+  introduction();
+  
+} */
+output:-
+MY name is soniya
+branch is cse
+CGPA is_
+______________________________________________________________
+
+/*#include <iostream>
+using namespace std;
+void introduction(string n,string branch,float cgpa){
+  cout<<"MY name is "<<n<<endl;
+  cout<<"branch is "<<branch<<endl;
+  cout<<"CGPA is "<<cgpa<<endl;
+}
+int main()
+{
+  introduction("soniya","cse",10.0);
+}*/
+output:-
+MY name is soniya
+branch is cse
+CGPA is 10.0
+_________________________________________________________________________
+#include <iostream>
+using namespace std;
+void summ(int a, int b) {
+    cout << a + b;
+}
+int suum(int a, int b) {
+    return a + b;
+
+}
+int main() {
+    summ(8, 2);
+    cout << endl;
+    int a = suum(9, 7);
+
+    cout << suum(9, 7);
+
+}
+output:-
+10
+16
+_________________________________________________________________________
+/*#include<iostream>
+using namespace std;
+int main() {
+	int num,factorial=1;
+	cout<<" Enter Number To Find Its Factorial:  ";
+	cin>>num;
+	for (int a=1;a<=num;a++) {
+		factorial=factorial*a;
+	}
+	cout<<"Factorial of Given Number is ="<<factorial<<endl;
+	return 0;
+}*/
+output:-
+Enter Number To Find Its Factorial:  5
+Factorial of Given Number is =120
+
+_________________________________________________________________
+  
