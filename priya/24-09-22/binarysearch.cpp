114081fbd989494a96fed25e4e1dea3450ffcbec
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-09-26 02:16:42 +0530

    Create binarysearch.cpp

---
+++ priya/24-09-22/binarysearch.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+void binarysearch(vector<int>arr,int element)
+
+{
+    sort(arr.begin(),arr.end());
+    int ans=-1;
+
+    int l=0,h=arr.size()-1;
+    while(l<=h)
+    {
+        int mid =(l+h)/2;
+        if(arr[mid]==element)
+        {
+            ans=1;
+            cout<<"element found at "<<mid<<"index"<<endl;
+            break;
+        }
+        else if(arr[mid]<element)
+        {
+            l=mid+1;
+        }
+        else
+        {
+            h=mid-1;
+        }
+        
+    }
+
+    if(ans==-1)
+    {
+        cout<<"element not found "<<endl;
+    }
+}
+int main()
+{
+   
+    binarysearch({23,34,44,55,67,78,87},44);
+   return 0;
+}
+
+
+
+
+output:-
+  
+element found at 2 index.  
