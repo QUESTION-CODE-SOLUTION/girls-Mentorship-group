280e8aec6367fee4c97ef0de0fbd533e861331b6
poonam <113535198+Poonamraut040@users.noreply.github.com>
2022-09-28 01:08:59 +0530

    Create leetcode duplicate no 26.cpp

---
+++ poonam/28-09-22/leetcode duplicate no 26.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+       int j=1;
+        for(int i=0; i< nums.size()-1;i++)
+        {
+            if( nums[i]!=nums[i+1])
+            {
+                nums[j]=nums[i+1];
+                    j++;
+            }
+        }    
+        
+        return j;
+    }
+};
+
+
+COMMENT:
+your input
+[1,1,2]
+Output
+[1,2]
+Expected
+[1,2]
