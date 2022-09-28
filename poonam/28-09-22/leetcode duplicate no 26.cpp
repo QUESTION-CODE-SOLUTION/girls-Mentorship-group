e42ec9da81e29a502d84f9f90dc17b1a9c3debd9
poonam <113535198+Poonamraut040@users.noreply.github.com>
2022-09-28 06:38:36 +0530

    Update leetcode duplicate no 26.cpp

--- poonam/28-09-22/leetcode duplicate no 26.cpp
+++ poonam/28-09-22/leetcode duplicate no 26.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
        int j=1;
-        for(int i=0; i< nums.size()-1;i++)
+        for(int i=0; i<=nums.size()-1;i++)
         {
             if( nums[i]!=nums[i+1])
             {
                 nums[j]=nums[i+1];
                     j++;
             }
+            arr[j] = arr[n-1];
         }    
         
         return j;
