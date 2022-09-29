3820e5bbef5f6bb70787203e190a74d2e01b5c31
SONIYAPATIDAR <110100877+SONIYAPATIDAR@users.noreply.github.com>
2022-09-29 20:25:49 +0530

    Add files via upload

---
+++ soniya/transpose.cpp
@@ -0,0 +1,47 @@
+1 2 3 4 
+5 6 7 8
+9 10 11 12 
+13 14 15 16
+
+1 5 9 13
+2 6 10 14
+3 7 11 15
+4 8 12 16
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+void transpose(vector<vector<int>>vec){
+
+    for(int i=0;i<vec.size();i++){
+        for (int j=0;j<i;j++){
+            int a = vec[i][j];
+            vec[i][j]=vec[j][i];
+            vec[j][i]=a;
+
+        }
+    }
+    for (int i=0;i<vec.size();i++){
+            for (int j=0;j<vec.size();j++){
+                    cout<<vec[i][j]<<" ";
+            }
+            cout<<endl;
+    }
+}
+
+int main(){
+    vector<vector<int>>vec ={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    for (int i=0;i<vec.size();i++){
+        for (int j=0;j<vec.size();j++){
+                cout<<vec[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    transpose(vec);
+}
+
+
+
+
