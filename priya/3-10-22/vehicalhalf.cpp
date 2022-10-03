317fbd61fa367295f2df725cd2ebb0319c8c362b
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-10-03 21:32:39 +0530

    Create vehicalhalf.cpp

---
+++ priya/3-10-22/vehicalhalf.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+                             //function to call private constructor
+//class
+class vehical
+{  
+    public:
+    int mileage;
+    int door;
+    vehical()
+    {
+        
+    }
+    void type(int a,int b)
+    {
+        cout<<"vehical has "<<a<<"black tyres "<<b<<" wnite tyres"<<"\n";
+    }
+    void type(int a)
+    {
+        cout<<"vehical has "<<a<<"tyres"<<"\n"
+    }
+    
+};
+class car
+
+
+//function
+int main()
+{
+  
+
+}
