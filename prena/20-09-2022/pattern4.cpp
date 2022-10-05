559948d880e8a8e8fbb9480d95cdea26eddd981f
PrernaRathore10 <113295862+PrernaRathore10@users.noreply.github.com>
2022-10-05 23:56:12 +0530

    Create pattern4.cpp

---
+++ prena/20-09-2022/pattern4.cpp
@@ -0,0 +1,40 @@
+// ******
+//  *****
+//   ****
+//    ***
+//     **
+//      *
+//     **
+//    ***
+//   ****
+//  *****
+// ******
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of lines : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for (int k=0;k<i-1;k++){
+            cout<<" ";
+        }
+        
+        for (int j=n-i+1;j>=1;j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=2;i<=n;i++){
+        
+        for (int j=n-i;j>=1;j--){
+            cout<<" ";
+        }
+        for (int k=1;k<=i;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
