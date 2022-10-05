d03b574fc42944d637ab8cc20f880b5384cb5475
shakti2002 <109802187+shakti2002@users.noreply.github.com>
2022-10-05 06:37:28 -0700

    Create pattern1.cpp

---
+++ shakti/19-09-2022/pattern1.cpp
@@ -0,0 +1,19 @@
+* * * * 
+* * * * 
+* * * * 
+* * * * 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+int i,j;
+for(int i=1; i<4; i++)
+{
+   for (int j=1; j<=4; j++)
+{cout<<" * ";
+}
+ cout<<endl;
+}
+  
+}
