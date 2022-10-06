5ad06b37ae91a02a091a6a6a80d5ad943e76911d
SONIYAPATIDAR <110100877+SONIYAPATIDAR@users.noreply.github.com>
2022-10-06 12:42:01 +0530

    Create vecdecrease.cpp

---
+++ soniya/20-09-2022/vecdecrease.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+  vector<int>vec={100,202,30,40,500,60,70};
+  sort(vec.begin(),vec.end(),greater<int>());
+  for(int i=0; i<vec.size(); i++){
+    cout<<vec[i]<<" ";
+
+  }
+  cout<<endl;
+}
+OUTPUT:---
+500 202 100 70 60 40 30 
