a84ef2d4d6823f99e62d0124ef5cf7562944a086
SONIYAPATIDAR <110100877+SONIYAPATIDAR@users.noreply.github.com>
2022-10-06 11:52:22 +0530

    Create tableanysquareno.cpp

---
+++ soniya/16-09-2022/tableanysquareno.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int n;
+ cout<<"Enter a number : ";
+ cin>>n;
+ for(int i=1; i<=n; i++)
+ {
+    cout<<"square of "<<i<<" is "<<i*i<<endl;
+
+ }
+ return 0;
+}
