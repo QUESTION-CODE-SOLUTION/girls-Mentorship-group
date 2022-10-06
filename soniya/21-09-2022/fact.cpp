9203d431c3d59a89feb717242ac7f08cd8541890
SONIYAPATIDAR <110100877+SONIYAPATIDAR@users.noreply.github.com>
2022-10-06 12:50:24 +0530

    Create fact.cpp

---
+++ soniya/21-09-2022/fact.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int num,factorial=1;
+	cout<<" Enter Number To Find Its Factorial:  ";
+	cin>>num;
+	for (int a=1;a<=num;a++) {
+		factorial=factorial*a;
+	}
+	cout<<"Factorial of Given Number is ="<<factorial<<endl;
+	return 0;
+}*/
+output:-
+Enter Number To Find Its Factorial:  5
+Factorial of Given Number is =120
