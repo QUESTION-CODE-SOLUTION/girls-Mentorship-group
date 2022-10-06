dbe8bf678df6c4ed6c99d253d974c364339cc50f
shakti2002 <109802187+shakti2002@users.noreply.github.com>
2022-10-05 19:14:48 -0700

    Create introfunction.cpp

---
+++ shakti/21-09-2022/introfunction.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void introdunction(string n, string branch,float CGPA)
+{
+    cout<<"my name is "<<n<<endl;
+    cout<<"my branch is "<<branch<<endl;
+    cout<<"cgpa is "<<CGPA<<endl;
+}
+int main(){
+    introdunction("shakti","aiads",8.48);
+}
