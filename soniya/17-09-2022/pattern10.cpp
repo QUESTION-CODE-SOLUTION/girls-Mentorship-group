2d5aa2c90ff6fc98faf7ef4917b850d768820544
SONIYAPATIDAR <110100877+SONIYAPATIDAR@users.noreply.github.com>
2022-10-06 12:14:29 +0530

    Create pattern10.cpp

---
+++ soniya/17-09-2022/pattern10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  for(int i= 1;i <= 5; i++){
+    for(int j= 1; j <= i; j++){
+      //if(i % 2 == 0 && j % 2 == 0){
+       cout<<j;
+     // }
+     // else{
+      // cout<<"*";
+      // }
+   }
+   cout<<endl;
+ }
+return 0;
+}
+OUTPUT................................................................
+1
+12
+123
+1234
+12345
