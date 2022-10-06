5c1ab97b10bad74f638629635a921483836d4510
shakti2002 <109802187+shakti2002@users.noreply.github.com>
2022-10-06 11:33:33 -0700

    Create find the duplicate number.cpp

---
+++ shakti/Leetcode/27-09-2022/find the duplicate number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int a;
+    sort(nums.begin(),nums.end());
+         for(int i=1;i<nums.size();i++){
+             if(nums[i]==nums[i-1]){      
+                 a=nums[i];
+                }
+            }
+      return a;
+    }
+};
