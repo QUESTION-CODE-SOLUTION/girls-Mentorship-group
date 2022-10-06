0a1b7732b0a3ba2b5ca0d4483698cb5e98323dfc
SONIYAPATIDAR <110100877+SONIYAPATIDAR@users.noreply.github.com>
2022-10-06 12:47:58 +0530

    Create fun1.cpp

---
+++ soniya/22-09-2022/fun1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void introduction()
+{
+  cout<<"MY name is soniya"<<endl;
+  cout<<"branch is CSE"<<endl;
+  cout<<"CGPA is __"<<endl;
+}
+int main() {
+  introduction();
+  
+} */
+output:-
+MY name is soniya
+branch is cse
+CGPA is_
