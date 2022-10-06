2f6f234fbb8024b60bd7de7170fb0dbfe41b672f
shakti2002 <109802187+shakti2002@users.noreply.github.com>
2022-10-05 20:25:43 -0700

    Create arraysize.cpp

---
+++ shakti/22-09-2022/arraysize.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int arr[10];
+    for(int i=0; i<10; i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0; i<10; i++)
+    { cout<<arr[i]<<endl;
+       
+        cout<<sizeof(arr[1])<<" ";
+        cout<<sizeof(arr)<<" ";
+        cout<<sizeof(arr)/sizeof(arr[0])<<endl;
+        }
+}
+
