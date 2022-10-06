fcaaf2b53d55a0559decea88569b3a9fdfb48fb0
shakti2002 <109802187+shakti2002@users.noreply.github.com>
2022-10-05 19:16:15 -0700

    Create functionsum.cpp

---
+++ shakti/21-09-2022/functionsum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void summ(int a, int b)
+{
+    cout<<a+b<<endl;
+    return;
+}
+int suum (int a, int b){
+    return a+b;
+}
+
+int main(){
+    summ(8,5);
+    int a =suum(7,2);
+cout<<suum(7,2);
+}
