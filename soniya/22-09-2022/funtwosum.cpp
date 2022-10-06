7d2c4dc375578180332c1226376121a5cea26b5f
SONIYAPATIDAR <110100877+SONIYAPATIDAR@users.noreply.github.com>
2022-10-06 12:49:29 +0530

    Create funtwosum.cpp

---
+++ soniya/22-09-2022/funtwosum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void summ(int a, int b) {
+    cout << a + b;
+}
+int suum(int a, int b) {
+    return a + b;
+
+}
+int main() {
+    summ(8, 2);
+    cout << endl;
+    int a = suum(9, 7);
+
+    cout << suum(9, 7);
+
+}
+output:-
+10
+16
