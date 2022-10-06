54eeaf0bca7e6e0218c76e322f688c903e14a0d1
shakti2002 <109802187+shakti2002@users.noreply.github.com>
2022-10-05 18:54:15 -0700

    Create pattern8.cpp

---
+++ shakti/19-09-2022/pattern8.cpp
@@ -0,0 +1,21 @@
+* 
+**
+***
+****
+*****
+
+#include<iostream>
+using namespace std;
+ 
+ int main(){
+    int i,j;
+    for (int i=1; i<=5; i++)
+    {
+        for (int j=1; j<=i; j++)
+        {
+            cout<<" * ";
+       }
+    cout<<endl;
+    }
+    
+ }
