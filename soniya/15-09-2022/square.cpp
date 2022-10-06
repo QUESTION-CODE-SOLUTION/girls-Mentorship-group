3fcede9aa918b79b842c80f34830111b08a0d7d5
SONIYAPATIDAR <110100877+SONIYAPATIDAR@users.noreply.github.com>
2022-10-06 11:45:56 +0530

    Create square.cpp

---
+++ soniya/15-09-2022/square.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, square;
+    
+    // Asking for input
+    cout << "Enter a number: ";
+    cin >> n;
+    
+    // Calculating square
+    square = n * n;
+    
+    // Displaying output
+    cout << "Square of " << n << " is: " << square;
+    
+    return 0;
+} 
+  
