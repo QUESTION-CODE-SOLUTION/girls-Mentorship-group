6b7d760b01d65c37d32117a65f976234b84132ee
shakti2002 <109802187+shakti2002@users.noreply.github.com>
2022-10-05 19:30:53 -0700

    Create arraysbas10tobase2.cpp

---
+++ shakti/23-09-2022/arraysbas10tobase2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int base10tobase2(int num)
+{
+    int ans=1, count=0;
+    while(num!=0){
+        int rem=num%2;
+        ans+=pow(10,count)*rem;
+        count++;
+        num=num/2;
+    }
+    return ans;
+}
+
+int main(){
+    cout<<base10tobase2(13);
+    
+}
