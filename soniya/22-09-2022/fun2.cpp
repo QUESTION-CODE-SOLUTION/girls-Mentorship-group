8625e8fa909fb9b3fd191268c8718fc2b2720078
SONIYAPATIDAR <110100877+SONIYAPATIDAR@users.noreply.github.com>
2022-10-06 12:48:43 +0530

    Create fun2.cpp

---
+++ soniya/22-09-2022/fun2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void introduction(string n,string branch,float cgpa){
+  cout<<"MY name is "<<n<<endl;
+  cout<<"branch is "<<branch<<endl;
+  cout<<"CGPA is "<<cgpa<<endl;
+}
+int main()
+{
+  introduction("soniya","cse",10.0);
+}
+output:-
+MY name is soniya
+branch is cse
+CGPA is 10.0
