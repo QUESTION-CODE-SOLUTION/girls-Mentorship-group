6d581acb5b0ead138a1121d90a4f5a8655021c53
Nupur Hardiya <113535580+NupurHardiya@users.noreply.github.com>
2022-10-09 16:58:27 +0530

    Create traverse,push_backvector.cpp

---
+++ nupur/23.09.2022/traverse,push_backvector.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+   vector<int>vec={10,20,30,40,50,60};
+    cout<<vec.size()<<endl;
+    for(int i= 0 ; i< vec.size() ; i++){
+        cout<<vec[i]<<" ";
+    }
+    cout<<endl;
+vec.push_back(100);
+cout<<vec.size()<<endl;
+vec.push_back(200);
+vec.push_back(300);
+for(int i= 0;i<vec.size();i++){
+cout<<vec[i]<<" ";
+}
+return 0;
+}
