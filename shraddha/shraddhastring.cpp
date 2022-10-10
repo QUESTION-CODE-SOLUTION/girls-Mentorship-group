4fe5ee23e743acf7039f5f2508d59eeee8afc205
shraddha tiwari <113535239+shraddhatiwarii@users.noreply.github.com>
2022-10-10 20:22:14 +0530

    Create shraddhastring.cpp

---
+++ shraddha/shraddhastring.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s="abcdef";
+    cout<<s<<endl;
+    cout<<s.length()<<endl;
+    cout<<s.size()<<endl;
+    s = s+"hijk";  //concatination - adding new string//
+    cout<<s<<endl;
+    return 0;
+}
+
+
