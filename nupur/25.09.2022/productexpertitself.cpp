6c15247f7b3596f39dee4238472f280d9b6bc07a
Nupur Hardiya <113535580+NupurHardiya@users.noreply.github.com>
2022-10-10 16:57:39 +0530

    Create productexpertitself.cpp

---
+++ nupur/25.09.2022/productexpertitself.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+void productexceptitself(vector<int>vec)
+{
+    vector<int>arr(vec.size(),0); 
+   // {
+    int a=1;
+    for(int i=0;i<vec.size();i++)
+    {
+        a=a*vec[i];
+    }
+     for(int i=0;i<arr.size();i++)
+    {
+       arr[i]=a/vec[i];
+    }
+    for(int i=0;i<arr.size();i++)
+    {
+    cout<<arr[i]<<" ";
+    }
+    }
+//}
+
+int main()
+{
+    productexceptitself({1,4,5,6,3});
+    return 0;
+}
+
+//output:-
+  
+//360 90 72 60 120   
+  
