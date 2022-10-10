d0be99de2ab73ad67404ae3cd13ad50ca924870d
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-10-10 18:54:08 +0530

    Create base10tobase2.cpp

---
+++ priya/21-09-22/base10tobase2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+ 
+ int base10tobase2(int num)
+ {
+     int ans=0, count=0;
+     while(num!=0)
+     {
+         int rem=num%2;
+         ans+=(pow(10,count)*rem);
+         count++;
+         num=num/2;
+     }
+     
+     return ans;
+ }
+ int main()
+ {
+     cout<<base10tobase2(6);
+    
+ }
+  
+
+OUTPUT:-
+ 110 
+  
+  
