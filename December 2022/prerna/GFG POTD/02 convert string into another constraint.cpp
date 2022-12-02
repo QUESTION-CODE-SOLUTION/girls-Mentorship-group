a166e4997e1dba67b577897f6050efd5acecad5a
PrernaRathore10 <113295862+PrernaRathore10@users.noreply.github.com>
2022-12-02 23:50:58 +0530

    Create 02 convert string into another constraint.cpp

---
+++ December 2022/prerna/GFG POTD/02 convert string into another constraint.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int isItPossible(string S, string T, int M, int N) {
+        int i = 0,j =0;
+        if(M!=N)
+        return 0;
+        while(i<M||j<N){
+            while(S[i]=='#')
+            i++;
+            while(T[j]=='#')
+            j++;
+            if(i>=M & j<N)
+            return 0;
+            if(i<M & j>=N)
+            return 0;
+            if(S[i]!=T[j])
+            
+            return 0;
+        else{
+            if(S[i]=='A'&& i<j)
+            return 0;
+            else  if(S[i]=='B'&& i>j)
+             return 0;
+        }
+            i++;
+            j++;
+            
+        }
+        return 1;
+        
+    }
+};
